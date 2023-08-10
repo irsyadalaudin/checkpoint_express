f3906bec62ba74354d5438e4a1a76f91a4e39c63
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-08-10 22:49:44 +0300

    adding route to test the middleware auth  in routes/homePage.js

--- routes/homePage.js
+++ routes/homePage.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const auth = require('../middlewares/userAuth');
+// const auth = require('../middlewares/userAuth');
 const homePageRouter = express.Router();
 
 /* GET home page. */
 homePageRouter.get('/', (req, res, next) => {
     res.render('homePage', { title: 'home page express' });
 });
+
+// route to test the middleware auth
+homePageRouter.get('/auth', auth ,(req, res, next) => {
+    res.send('you are authorized');
+})
 
 module.exports = homePageRouter;
