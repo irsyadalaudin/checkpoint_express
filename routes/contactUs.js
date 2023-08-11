d5038dc28a97e92ac7b0cdeb7fef2ae8d2fb9124
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-08-11 10:38:12 +0300

    adding comment in contactUs.js

--- routes/contactUs.js
+++ routes/contactUs.js
@@ -1,9 +1,10 @@
+// Import the 'express' module to create a router
 const express = require('express');
 const contactUsRouter = express.Router();
 
 /* GET users listing. */
-contactUsRouter.get('/', (req, res, next) => {
-	res.render('contactUs', {title: 'contact us'});
-});
+contactUsRouter.get('/', (req, res, next) => {			// Define a route for handling GET requests to the '/contactUs' URL
+	res.render('contactUs', {title: 'contact us'});	    // Handle the GET request by rendering the 'contactUs' view (template)
+});													    // and passing the title 'contact us' to the view
 
-module.exports = contactUsRouter;
+module.exports = contactUsRouter;						// Export the contactUsRouter so it can be used in other parts of the application
