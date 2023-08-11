8d32c4ad77ff9dced9a97869333dbef22a09469c
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-08-11 10:56:21 +0300

    adding comment in ourService.js

--- routes/ourService.js
+++ routes/ourService.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const ourServiceRouter = express.Router();
 
-/* GET users listing. */
-ourServiceRouter.get('/', (req, res, next) => {
-	res.render('ourService', {title: 'our service'});
-});
+/* GET ourService page */
+ourServiceRouter.get('/', (req, res, next) => {		    // Define a route for handling GET requests to the '/ourService' URL
+	res.render('ourService', {title: 'our service'});	// Handle the GET request by rendering the 'ourService' view (template)
+});													    // and passing the title 'our service' to the view
 
-module.exports = ourServiceRouter;
+module.exports = ourServiceRouter;						// Export the ourServiceRouter so it can be used in other parts of the application
